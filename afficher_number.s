cbb1293c8b71a8d6ddacb20f6b973e8b4eff7241
Tom Delmaire <tom.delmaire@oteria.fr>
2026-02-08 15:13:19 +0100

    première version + test avec 9

---
+++ afficher_number.s
@@ -0,0 +1,20 @@
+global main:
+
+write_digit:
+  add rdi, 0
+  mov rax, 1
+  mov rsi, rdi
+  push rdi
+  mov rdi, 1
+  mov rdx, 1
+  mov rsi, rsp
+  syscall
+  pop rdi
+  ret
+
+main:
+  mov rdi, 9
+  call write_digit
+  mov rax, 60
+  xor rdi, rdi
+  syscall
